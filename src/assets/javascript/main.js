0f5eb306af9f25b6abdcb4f989d15ffd15749ced
Boon_Lee <Boon.Lee1@dell.com>
2022-11-01 15:05:07 +0800

    initial commit

--- src/assets/javascript/main.js
+++
@@ -1,9 +0,0 @@
-function triggerButton(){
-    console.log("Button Clicked!!!");
-
-    if ('caches' in window){
-        console.log("Cache");
-    } else {
-        console.log("Network");
-    }
-}
